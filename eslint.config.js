cfa4775dd5c06a78674e5294f1aca40a7d18c237
Frances.Jiang <frances.bh.jiang@gmail.com>
2025-06-09 17:20:48 +0800

    style(custom): update config of eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,7 @@ module.exports = [
       'prettier/prettier': 'error', // 违反 Prettier 规则就报错
       semi: ['error', 'always'], // 强制加分号
       quotes: ['error', 'single'], // 强制用单引号
+      indent: 'off', // 关闭 ESLint 的缩进检查
     },
 
     plugins: {
--- prettier.config.js
+++ prettier.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   semi: true, // 每行末尾加分号
   singleQuote: true, // 使用单引号
-  printWidth: 100, // 每行最多100字符换行
+  printWidth: 100, // 每行最多100字符换行，大于默认的80
+  useTabs: false, // 使用空格缩进而不是制表符
   tabWidth: 2, // 缩进用2个空格
   trailingComma: 'all', // 尾随逗号
   bracketSpacing: true, // 对象大括号内空格
+  arrowParens: 'always', // 箭头函数参数总是加括号
+  endOfLine: 'lf', // 换行符使用 LF
 };
